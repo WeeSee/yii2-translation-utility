a1539050fff24bc0c76ec98d6891c8c088bd1d97
WeeSee <weesee@web.de>
2017-09-12 12:48:36 +0200

    google translate service added

--- translate.php
+++ translate.php
@@ -1,8 +1,12 @@
 <?php
 
+use Stichoza\GoogleTranslate\TranslateClient;
+require 'vendor/autoload.php';
+
 $messagePath = "./messages";
 $sourceLanguage = "de";
 $targetLanguage = "en";
+$useGoogletranslate = false;
 
 function getInput($text,$default) {
 	readline_clear_history();
@@ -13,9 +17,18 @@ function getInput($text,$default) {
 	return $line;
 }
 
+function googleTranslate($sourceLanguage,$targetLanguage,$text) {
+	$translateClient = new TranslateClient($sourceLanguage,$targetLanguage); 
+	return $translateClient->translate($text);
+}
+
+
 $messagePath = getInput("Path to Yii2-message directory",$messagePath);
 $sourceLanguage = getInput("Source language (de)",$sourceLanguage);
 $targetLanguage = getInput("Target language (de)",$targetLanguage);
+$useGoogletranslateAnswer = getInput("Use Google Translate service (y/N)","y");
+if ($useGoogletranslateAnswer=="y")
+	$useGoogletranslate = true;
 
 
 $files = glob($messagePath."/".$sourceLanguage.'/*.php', GLOB_BRACE);
@@ -40,14 +53,24 @@ foreach($files as $file)
 		}
 		foreach($sourceMessages as $key => $value) {
 			echo "---\n";
-			echo "--> [$key]\n";
-			echo "$sourceLanguage: [$value]\n";
+			echo str_pad("-->",11)." [$key]\n";
+			echo str_pad($sourceLanguage,10).": [$value]\n";
 			if (!isset($targetMessages[$key]))
 				$targetMessages[$key]="";
-			echo "$targetLanguage: [".$targetMessages[$key]."]\n";
-			$antwort = getInput("New (Return->OK/.->End)",$targetMessages[$key]);
+			echo str_pad($targetLanguage,10).": [".$targetMessages[$key]."]\n";
+			if ($useGoogletranslate) {
+				$googleTranslated = googleTranslate($sourceLanguage,$targetLanguage,$value);
+				echo str_pad("google",10).": $googleTranslated\n";
+			}
+			$antwort = getInput($useGoogletranslate 
+				? "New (Return=>OK/-=>Google/.=>End)"
+				: "New (Return=>OK/.=>End)",
+				$targetMessages[$key]);
 			if ($antwort == ".") break;
-			$targetMessages[$key] = $antwort;
+			if ($useGoogletranslate && $antwort == "-") 
+				$targetMessages[$key] = $googleTranslated;
+			else 
+				$targetMessages[$key] = $antwort;
 		}
 		echo "New translation file:\n";
 		$newTargetContent = "<?php\n";
